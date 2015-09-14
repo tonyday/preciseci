a103f21676e226e59a00ad86ea7dd35083846696
Tony Day <tony@lazylabs.com>
2015-09-15 05:09:12 +1200

    Change to use simple vimrc and separate Vundle plugin list.

--- 60_vim_and_vundle.sh
+++ 60_vim_and_vundle.sh
@@ -1,6 +1,8 @@
 # Setup vim / vundle
-# get a vimrc that has a list of plugins to be installed
-curl -o ~/.vimrc https://gist.githubusercontent.com/tonyday/bc4062594af2ad06b140/raw/4083d63a47dc4e75af66eafbd511b73bcd5c7418/install_vimrc
+# Get the list of Vundle plugins to be installed
+curl -o ~/.vim_vundles.vim https://raw.githubusercontent.com/tonyday/vudotfiles/master/dots/vim_vundles.vim
+# Get a simple vimrc that includes the list of plugins to be installed
+curl -o ~/.vimrc https://raw.githubusercontent.com/tonyday/vudotfiles/master/dots/vimrc_install
 # get vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # start vim and execute vundle install
